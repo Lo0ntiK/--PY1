3e41d3adecba4bd68386bc87e539fd8ef21a242b
Lo0ntiK <115427046+Lo0ntiK@users.noreply.github.com>
2022-10-29 12:46:24 +0300

    Update task_2.py

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -1,11 +1,17 @@
 def get_count_char(str_):
     dict_chars = {}
     default_val = 0
-    char_list = list(str_.lower())
-    for char in char_list:
+    for char in str_.lower():
         if char.isalpha():
             dict_chars[char] = dict_chars.get(char,default_val) + 1
     return dict_chars
+
+def percents(dict_chars):
+    summ = sum(dict_chars.values())
+    new_dict_chars = {}
+    for char in dict_chars.keys():
+        new_dict_chars[char] = dict_chars.get(char,0)/summ * 100
+    return new_dict_chars
 
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
@@ -13,4 +19,4 @@ main_str = """
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
-
+print(percents(get_count_char(main_str)))
