ec9448da747819f22b9d7c0a898adf16d35253a5
Lo0ntiK <115427046+Lo0ntiK@users.noreply.github.com>
2022-11-15 21:36:48 +0300

    Update task_3.py

--- Лабораторная работа 5/task_3.py
+++ Лабораторная работа 5/task_3.py
@@ -1,20 +1,15 @@
-from random import randint, shuffle
+from random import randint
 
 
 def get_unique_list_numbers() -> list[int]:
     list_ = []
-    for _ in range(15):
-        while True:
-            number = randint(-10,10)
-            if not number in list_:
-                list_.append(number)
-                break
-            else:
-                continue
+    while len(list_) != 15:
+        number = randint(-10,10)
+        if not number in list_:
+            list_.append(number)
     return list_
 
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
-
