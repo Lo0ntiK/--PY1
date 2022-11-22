a401831fe89f5bbec5872fcef02cfb0c59e0c1ee
Lo0ntiK <115427046+Lo0ntiK@users.noreply.github.com>
2022-11-22 23:14:01 +0300

    Update task_4.py

--- Лабораторная работа 6/task_4.py
+++ Лабораторная работа 6/task_4.py
@@ -1,4 +1,3 @@
-
 import json
 
 INPUT_FILE = "input.csv"
@@ -6,27 +5,21 @@ INPUT_FILE = "input.csv"
 
 def csv_to_list_dict(filename: str, delimiter: str = ',', new_line: str = '\n') -> list[dict]:
     with open(filename) as fi:
-        first_line = True
-        rows = []
-        headers = []
+        input_str = ''
         for line in fi:
-            if first_line:
-                headers += str(line[:len(line)-1]).split(',')
-            else:
-                if line == '':
-                    continue    # пустые строки не нужны
-                row = str(line[:len(line)-1]).split(',')
-                rows.append(row)
-            first_line = False
-    
+            input_str += line
+        rows = input_str.split(new_line)
+        headers, rows = rows[:1], rows[1:]
+        headers = headers[0].split(delimiter)
+        rows = [i.split(delimiter) for i in rows if i != '']
+
     list_of_dicts = []
     length = len(headers)
     for row in rows:
         dict_ = {headers[i]: row[i] for i in range(length)}
         list_of_dicts.append(dict_)
-        
+
     return list_of_dicts
 
 
 print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
-
